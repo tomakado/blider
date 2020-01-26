e7c967ba40fc9884cfbd5199e4a8e36bd3530774
Ildar Karymov <hi@ildarkarymov.ru>
2020-01-26 18:25:29 +0700

    Configuration module documented

--- config/config.go
+++ config/config.go
@@ -11,12 +11,24 @@ import (
 )
 
 type Config struct {
-	Period           Period `json:"period,omitempty"`
+	// Period is a time interval between fetch & change iterations.
+	Period Period `json:"period,omitempty"`
+	// LocalStoragePath is path to local directory used as images storage.
 	LocalStoragePath string `json:"local_storage_path"`
-	DBPath           string `json:"db_path"`
-	MaxFetchPages    int    `json:"max_fetch_pages"`
+	// DBPath is path to SQLite databse.
+	DBPath string `json:"db_path"`
+	// MaxFetchPages is maximum number of pages to look at.
+	// This parameter is being passed to provider and
+	// can be changed in runtime. For example, SimpleDesktopsProvider
+	// changes it on each iteration to optimize next
+	// wallpaper search.
+	MaxFetchPages int `json:"max_fetch_pages"`
 }
 
+// FromFile tries to load configuration from JSON file.
+// If some of configuration fields have wrong or empty values
+// FromFile sets default values.
+// Returns Config instance on success and error on failure.
 func FromFile(filename string) (*Config, error) {
 	f, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -47,9 +59,13 @@ func FromFile(filename string) (*Config, error) {
 	return c, nil
 }
 
+// Period is a string in format "<integers>(s|m|h)"
+// representing time duration. For example: 12h, 30s
+// or 5h. Integer in period must be positive.
 type Period string
 
-// ToTime transforms string in format "<numbers>(s|m|h)" to Duration instance
+// ToTime transforms Period to time.Duration.
+// Returns time.Duration on success and error on failure.
 func (p *Period) ToTime() (time.Duration, error) {
 	pStr := strings.TrimSpace(string(*p))
 	numVal, err := strconv.Atoi(pStr[:len(pStr)-1])
